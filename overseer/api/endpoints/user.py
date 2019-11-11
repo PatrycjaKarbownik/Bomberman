769e2940e461b826c3e93ebd0087cae2349c6a02
Temebe <seboado@gmail.com>
2019-11-11 17:32:48 +0100

    feat: add structures for translation

--- overseer/api/endpoints/user.py
+++ overseer/api/endpoints/user.py
@@ -5,6 +5,9 @@ from flask_restplus import Resource
 import engine.lobby as lobby
 from api import models
 from api.restful import api
+from api.translation_manager import translate as tr
+from api.translation_manager import Language
+from messages import message_codes as message
 
 logger = logging.getLogger(__name__)
 
@@ -36,8 +39,8 @@ class UserLogin(Resource):
         username = api.payload['username']
         if lobby.user_exists(username):
             return {
-                'type': 'error',
-                'errorCode': 'login-already-used'
-            }, 401
+                       'type': 'error',
+                       'errorMessage': tr(message.error_login_already_used, Language.POLISH)
+                   }, 401
         user_id = lobby.add_user(username)
         return user_id, 200
