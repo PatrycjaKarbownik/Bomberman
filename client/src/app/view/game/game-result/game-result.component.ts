3b48caec5e68c3c0e424e9fb1e4c31ad581ea2fe
PatrycjaKarbownik <patrycja@karbownik.net>
2019-12-18 12:58:49 +0100

    feat: add game module

---
+++ client/src/app/view/game/game-result/game-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bomb-game-result',
+  templateUrl: './game-result.component.pug',
+  styleUrls: ['./game-result.component.scss']
+})
+export class GameResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/view/game/game-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewModel } from '@app/core/navigation/view.model';
+import { GameComponent } from '@app/view/game/game/game.component';
+import { GameResultComponent } from '@app/view/game/game-result/game-result.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: ViewModel.GAME, pathMatch: 'full' },
+  { path: ViewModel.GAME, component: GameComponent },
+  { path: ViewModel.GAME_RESULT, component: GameResultComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class GameRoutingModule {}
