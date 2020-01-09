5e94ef1c3fb72fcfa2e89a0a40bf21d21cdd7b13
PatrycjaKarbownik <patrycja@karbownik.net>
2020-01-09 17:32:59 +0100

    feat: add rendering map and hero

--- client/src/app/view/game/match/match.component.ts
+++ client/src/app/view/game/match/match.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MapModel } from '@app/view/game/models/map.model';
+import { GameService } from '@app/view/game/game.service';
+import { TileType } from '@app/view/game/models/tile-type.model';
+import { CornerType } from '@app/view/game/models/corner-type.model';
+
 @Component({
   selector: 'bomb-match',
   templateUrl: './match.component.pug',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchComponent implements OnInit {
 
-  constructor() { }
+  private map: MapModel;
+
+
+  TileType = TileType;
+  CornerType = CornerType;
+
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+    this.map = this.gameService.getMap();
+  }
+
+  getCornerType(): CornerType {
+    let userInGameId = 3;
+
+    return userInGameId % 4;
   }
 
 }
---
+++ client/src/app/view/game/models/corner-type.model.ts
@@ -0,0 +1,7 @@
+// using to determine hero's position
+export enum CornerType {
+  LEFT_TOP,
+  RIGHT_TOP,
+  LEFT_BOTTOM,
+  RIGHT_BOTTOM
+}
